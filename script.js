6bd7052aaeac5ae1868ba8dea6d46faac51f8c5c
GchatChevere <chev0050@student.acitech.org>
2025-02-28 09:17:07 -0500

    Got alert to popup! "OUCHHHH" (Hehehe)

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const button1 = document.getElementById("button1");
 
-button1.addEventListener("click" , yellatuser);
+/*const message = document.getElementById("response")
+function yellatuser(click){
+    response.textcontent = "ouch!";
+    console.log(response)
 
-function yellatuser() {
-    alert("STOP PRESSING ME!");
+
 }
+button1.addEventListener("click" , yellatuser);*/
+
+const button1 = document.getElementById("button1")
+
+button1.addEventListener("click", yell_at_user)
+
+function yell_at_user() {
+        alert("Ouch!");
+    }
