b9492bca78bf5a6cf5ce4903d545a9295359c886
GchatChevere <chev0050@student.acitech.org>
2025-03-03 09:15:32 -0500

    Got text to work for button added different senarios

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const button1 = document.getElementById("button1")
-let response = document.getElementById("response")
 
-button1.addEventListener("click" , yell_at_user)
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let text = document.getElementById("response");
 
-function yell_at_user() {
-    alert("Ouch"+"!");
+let clicks = 0;
+
+button1.addEventListener("click" , annoy);
+
+function annoy() {
+    clicks += 1
+    if(clicks == 5) {
+        text.innerHTML = "Ouch!";
+    }
+
+    if(clicks == 9) {
+        text.innerHTML = "I swear to clicky click click";
+    }
+
+    if(clicks == 14) {
+        text.innerHTML = "that was the last click";
+    } 
+
+    if(clicks == 19) {
+        text.innerHTML = "";
+    } 
+
+    if(clicks == 20) {
+        button1.innerHTML = "Recalibrating";
+    } 
+
+    if(clicks == 29) {
+        button1.innerHTML = "Recalibration Finished";
+    }   
+    if(clicks == 30) {
+        button1.innerHTML = button2.innerHTML;
+    } 
+ 
 }
-    
-    /*for (let click = 1; click <= "!"; click = "!"+"!") 
-        console.log("!" + 1);
-}*/
