05fa19ea83e0b704a7380c9ff0b927a93da099d3
GchatChevere <chev0050@student.acitech.org>
2025-03-05 09:10:55 -0500

    made my button move when you click it! also fixed alot of stuff.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let button1 = document.getElementById("button1");
+let buttonbase = document.getElementById("buttonbase");
 let text = document.getElementById("response");
 let button2 = document.getElementById("button2");
 let text1 = document.getElementById("response2");
@@ -12,39 +13,67 @@ button1.addEventListener("click" , annoy);
 
 function annoy() {
     clicks += 1
-    if(clicks == 5) {
+    if(clicks == 1) {
         text.innerHTML = "Ouch!";
     }
     
-    else if(clicks == 9) {
-        text.innerHTML = "I swear to clicky click click";
+    else if(clicks == 2) {
+        text.innerHTML = "I Swear To Clicky Click Click";
     }
     
-    else if(clicks == 14) {
-        text.innerHTML = "that was the last click";
+    else if(clicks == 3) {
+        text.innerHTML = "That Was The Last Click";
     } 
     
-    else if(clicks == 19) {
+    else if(clicks == 4) {
         text.innerHTML = "";
     } 
     
-    else if(clicks == 20) {
+    else if(clicks == 5) {
         button1.innerHTML = "Recalibrating";
     } 
     
-    else if(clicks == 29) {
-        button1.style.backgroundColor = "#00FF00";
-        button1.style.borderColor ="yellow"
+    else if(clicks == 6) {
+        button1.classList.add ("happy");
+        button1.classList.add ("glow");
         button1.innerHTML = "Recalibration Finished";
     }   
-    else if(clicks == 34) {
-        button1.style.backgroundColor = "#00FF00";
-        button1.style.borderColor ="black"
-    }   
-    else if(clicks == 32) {
-    }   
+    else if(clicks == 7) {
+            button1.classList.remove ("glow");  
+        button1.innerHTML=" "
+    }  
     
-    else if(clicks == 40) {
-        text.innerHTML = "if you touch me one more time i swear i will murder your family" ;
-    }   
+    else if(clicks == 8) {
+        text.innerHTML = "Hey That Tickles!" ;
+    }
+
+    else if(clicks == 9) {
+        buttonbase.classList.add ("move1")
+        button1.classList.add ("movement1");
+        text.innerHTML = "Catch Me If You Can!" ;
+
+    }  
+
+    else if(clicks == 10) {
+        button1.classList.add ("movement2");
+        buttonbase.classList.add("move2")
+    } 
+    
+    else if(clicks == 11) {
+        button1.classList.add ("movement3");
+        buttonbase.classList.add("move3")
+    }
+
+    else if(clicks == 12) {
+        button1.classList.add ("movement4");
+        buttonbase.classList.add("move4")
+        text.innerHTML = "OKAY IM DONE"
+    }
+
+    else if(clicks == 12) {
+        button1.classList.add ("movement4");
+        buttonbase.classList.add("move4")
+        text.innerHTML = "OKAY IM DONE"
+    }
+
 }
